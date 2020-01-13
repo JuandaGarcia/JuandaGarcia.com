b2e166e9f29c1b7bb13248020b09a5ee80be256f
Juan David <gottygarcia@gmail.com>
2020-01-12 22:12:53 -0500

    Avances en seccion de proyectos :D

--- src/pages/Proyectos.js
+++ src/pages/Proyectos.js
@@ -1,8 +1,36 @@
 import React from 'react'
-import ProyectosSection from '../components/ProyectosSection'
+import Flores from '../img/flores.svg'
+import Web from '../img/Macbook.png'
+import Movile from '../img/movil.png'
+import { Link } from 'react-router-dom'
 
 function Proyectos() {
-	return <ProyectosSection />
+	return (
+		<div className="ProyectosSection">
+			<div className="movile-web-proyects">
+				<Link className="card-proyecto noSelect card-1">
+					<div className="info-card">
+						<h1>Web</h1>
+						<p>10 Projects</p>
+					</div>
+					<img src={Web} alt="Web" />
+				</Link>
+				<Link className="card-proyecto noSelect card-2">
+					<div className="info-card">
+						<h1>Móvil</h1>
+						<p>10 Projects</p>
+					</div>
+					<img src={Movile} alt="Móvile" />
+				</Link>
+			</div>
+			<figure className="flores-projects-vuelta">
+				<img src={Flores} alt="flores" />
+			</figure>
+			<figure className="flores-projects">
+				<img src={Flores} alt="flores" />
+			</figure>
+		</div>
+	)
 }
 
 export default Proyectos
